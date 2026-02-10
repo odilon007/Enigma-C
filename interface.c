6b68ef3dc2146fb438d65802be3aa8db7be26f1c
Odilon <odilonpontes7@gmail.com>
2026-02-10 19:55:58 -0300

    cadeado em ASCII

--- interface.c
+++ interface.c
@@ -1,30 +1,43 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "interface.h"
 
+// Cores ANSI
+#define GREEN "\033[1;32m"
+#define RESET "\033[0m"
+
 void exibirLogo(void) {
-    printf("#########################################################################\n");
-    printf("##                                                                     ##\n");
-    printf("##  #######  ###   ## ##  ######  ###   ###  #####           ######    ##\n");
-    printf("##  ##       ####  ## ## ##       #### #### ##   ##        ##         ##\n");
-    printf("##  #####    ## ## ## ## ##  ###  ## #### ## #######  ##### ##         ##\n");
-    printf("##  ##       ##  #### ## ##   ##  ##  ##  ## ##   ##        ##         ##\n");
-    printf("##  #######  ##   ### ##  ######  ##      ## ##   ##        ######     ##\n");
-    printf("##                                                                     ##\n");
-    printf("##            SISTEMA DE CRIPTOGRAFIA DE ARQUIVOS EM C                  ##\n");
-    printf("##                    DESENVOLVIDO EM LINGUAGEM C                      ##\n");
-    printf("##                                                                     ##\n");
-    printf("#########################################################################\n");
+    printf(GREEN);
+    printf("        .--------.\n");
+    printf("       / .------. \\\n");
+    printf("      / /        \\ \\\n");
+    printf("      | |        | |\n");
+    printf("     _| |________| |_\n");
+    printf("   .' |_|        |_| '.\n");
+    printf("   '._____      _____.'   " "  XOR CRYPTO SYSTEM  "  "\n");
+    printf("   |     |______|     |   " "Protecao de Arquivos "  "\n");
+    printf("   |     |  __  |     |   " "   em Linguagem C    "  "\n");
+    printf("   |     | |  | |     |\n");
+    printf("   |     | |__| |     |\n");
+    printf("   |     |______|     |\n");
+    printf("   '------------------'\n");
+    printf(RESET);
+    printf("\n");
 }
 
 
 
 void exibirMenu(void) {
-    printf("\n[1] Criptografar XOR\n");
-    printf("[2] Descriptografar XOR\n");
-    printf("[0] sair\n\n");
-    printf("_________________________________________________________________________\n");
+    printf("  [MENU PRINCIPAL]  " "\n");
+    printf("  "  "[1]"  " Criptografar Arquivo (XOR)\n");
+    printf("  "  "[2]"  " Descriptografar Arquivo (XOR)\n");
+    printf("  "  "[0]"  " Sair do Sistema\n");
+    printf( "________________________________________________________" "\n");
+    printf("Escolha uma opcao: ");
 }
 
+
+
 void opcaoInvalida(void) {
-    printf("Opção inválida\n\n");
+    printf("\n" "[!] Opcao invalida. Tente novamente.\n\n");
 }
+
